8cbc7f1211c0eec8aec74588e894b34db65f081c
Vladislav <novitsky.vladislav00@gmail.com>
2020-10-18 01:31:50 +0300

    Added all of the fields info

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -4,19 +4,55 @@ import 'package:mobile_meep/widgets/input.dart';
 
 class InputPage extends StatelessWidget {
   final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
-  final List<String> descriptionsText = ['x of the cell', 'y of the cell'];
-  final Map attributes = {
-    'cell': ['x', 'y']
+  final Map descriptionsText = {
+    'cell': ['x of the cell', 'y of the cell', 'z of the cell'],
+    'geometry': {
+      'coordinates': [
+        'x of the geometry',
+        'y of the geometry',
+        'z of the geometry'
+      ],
+      'center': [
+        'x of the geometry center',
+        'y of the geometry center',
+      ],
+      'material': ['epsilon'],
+    },
+    "sources": {
+      "frequency": ['sources_frequency'],
+      "center": ['sources_x', 'sources_y']
+    },
+    "simulation_time": {
+      "between": ['simulation_time_between'],
+      "until": ['simulation_time_until']
+    },
+    "pml_layers": ['pml_layers'],
+    "resolution": ['resolution'],
   };
-  final Map validators = {
-    'cell': {
-      'x': [
-        FormBuilderValidators.numeric(),
+  final Map attributes = {
+    'cell': ['cell_x', 'cell_y', 'cell_z'],
+    'geometry': {
+      'coordinates': [
+        'geometry_coordinates_x',
+        'geometry_coordinates_y',
+        'geometry_coordinates_z',
       ],
-      'y': [
-        FormBuilderValidators.numeric(),
+      'center': [
+        'geometry_center_x',
+        'geometry_center_y',
       ],
-    }
+      'material': ['geometry_material']
+    },
+    "sources": {
+      "frequency": ['sources_frequency'],
+      "center": ['sources_x', 'sources_y']
+    },
+    "simulation_time": {
+      "between": ['simulation_time_between'],
+      "until": ['simulation_time_until']
+    },
+    "pml_layers": ['pml_layers'],
+    "resolution": ['resolution'],
   };
 
   @override
@@ -24,10 +60,7 @@ class InputPage extends StatelessWidget {
     return SafeArea(
       child: Container(
         alignment: Alignment.center,
-        child: Card(
-          child: InputField(this._fbKey, this.descriptionsText, this.attributes,
-              this.validators),
-        ),
+        child: InputField(this._fbKey, this.descriptionsText, this.attributes),
       ),
     );
   }
